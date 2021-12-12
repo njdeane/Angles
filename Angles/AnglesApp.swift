1de4cc6746e3040a545152de695b95f54420791e
Nic Deane <njdeane7@gmail.com>
2021-12-12 14:51:22 +1000

    Initial Commit

---
+++ Angles/AnglesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnglesApp.swift
+//  Angles
+//
+//  Created by Nic Deane on 12/12/21.
+//
+
+import SwiftUI
+
+@main
+struct AnglesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
